6f9e512687a8e8d78b86ffd664bd59b3ed83fab7
KFoobar <david@kfoobar.se>
2020-01-31 19:24:31 +0100

    First commit

---
+++ README.md
@@ -0,0 +1,14 @@
+# Simple placeholder page
+
+Landing page for blank domains.
+
+## About
+
+This is a simple and clean landing page that I use as placeholder for blank domains until I'm ready to push the the "real" webpage to production.
+Particularly useful for newly created sites on Laravel Forge that prints `phpinfo()` on the default page.
+
+You a free to use it as well if you find it useful. Contributions and ideas are welcome!
+
+## License
+
+The MIT License (MIT). Please see [License File](LICENSE.md) for more information.
---
+++ public/index.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="sv">
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        <meta name="description" content="This page is empty, because we haven't uploaded anything yet">
+        <meta name="robots" content="noindex, nofollow">
+        <title>This page is empty!</title>
+        <link rel="stylesheet" href="//fonts.googleapis.com/css?family=Montserrat:400,700&display=swap">
+        <link rel="stylesheet" href="/style.css">
+        <link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon.png">
+        <link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png">
+        <link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png">
+        <link rel="manifest" href="/site.webmanifest">
+    </head>
+    <body>
+        <div class="flex-container">
+            <div class="row">
+                <div class="flex-item">
+                    <h1>Page not found</h1>
+                    <h2>Because we haven't uploaded anything yet</h2>
+                </div>
+            </div>
+        </div>
+    </body>
+</html>
